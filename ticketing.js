7617626f4a508681e3b13e5c23d48c05b68be6c7
psullivan01 <psullivan@hearstautos.com>
2017-11-21 20:30:43 -0500

    final draft

--- ticketing.js
+++ ticketing.js
@@ -1,9 +1,78 @@
-const Sequelize = require('sequelize')
 
-let express = require('express')
-let bodyParser = require('body-parser')
+$(function() {
+	$.ajax({
+		method: 'GET',
+		url: 'http://localhost:3000/users',
+		success: function(res) {
+			console.log(res)
+			for (var i = 0; i < res.length; i++) {
+				console.log(res[i])
+				$("#selectUser").append($('<option>', {
+					value: res[i],
+					text: res[i]
+				}))
+			}
+		}
+	})
 
-let app = express()
-let parser = bodyParser.json()
+	$.ajax({
+		method: 'GET',
+		url: 'http://localhost:3000/tickets',
+		success: function(res) {
+			console.log(res)
+			for (var y = 0; y < res.length; y++) {
+				console.log("this is the ticket:", res[y])
+				$("#ticketList").append("<li>" + res[y] + "</li>")
+			}
+		}
+	})
 
-app.use(parser)
+
+	$('#userSubmit').click(function(e) {
+		e.preventDefault()
+		var firstName = $('#firstName').val()
+		var lastName = $('#lastName').val()
+
+		console.log(firstName)
+
+		$.ajax({
+			method: 'POST',
+			url: 'http://localhost:3000/users',
+			data: {
+				first_name : firstName,
+				last_name : lastName
+			},
+			success: function() {
+				console.log('it worked')
+			}
+		})
+	})
+
+	$('#ticketSubmit').click(function(e) {
+		e.preventDefault()
+
+		var ticketUser = $('#selectUser').val()
+		var ticketDescription = $('#body-field').val()
+		var ticketDate = $("#example-date-input").val()
+
+		console.log('clicked!', ticketUser, ticketDescription, ticketDate)
+
+		$.ajax({
+			method: 'POST',
+			url: 'http://localhost:3000/tickets',
+			data: {
+				ticket_desc: ticketDescription,
+				ticket_due_date: ticketDate,
+				ticket_user: ticketUser
+			},
+			success: function() {
+				console.log('it worked')
+			}
+		})
+	})
+
+})
+
+
+
+
